c5c37c7c2db4f2bc79c19323057c291643cc16cb
Akio Yamazaki <mray197835@gmail.com>
2018-01-28 13:28:48 +0900

    [add] init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './Board';
+import { observe } from './Game';
+
+const rootEl = document.getElementById('root');
+
+observe(knightPosition =>
+  ReactDOM.render(
+    <Board knightPosition={knightPosition} />,
+    rootEl
+  )
+);
+
